f63199b63b19424cf8a49af99ded6755f4b718a5
James-Edward-Marin-Noriega <james.marin.noriega@gmail.com>
2025-06-12 00:28:10 -0600

    Update 6_delete_data.py

--- 6_delete_data.py
+++ 6_delete_data.py
@@ -1,1 +1,12 @@
+import sqlite3
+
+conn = sqlite3.connect("ejemplo.db")
+cursor = conn.cursor()
+
+# Eliminamos un registro
+cursor.execute("DELETE FROM usuarios WHERE nombre = ?", ("Ana",))
+
+conn.commit()
+print("Datos eliminados.")
+conn.close()
 
